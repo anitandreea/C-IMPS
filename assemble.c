6520b58e3a8d798f80a12e0623f28bf966fba232
James Simpson <js2310@doc.ic.ac.uk>
2011-06-02 14:48:03 +0100

    Added standard libraries and a main function.

--- assemble.c
+++ assemble.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
 void writeFile( char *filename, int *instructions, int fileSize ) {
 	FILE *fp = fopen(filename,"wb");
 	fwrite(instructions, sizeof(int), fileSize, fp);
 	fclose(fp);
+}
+
+
+
+int main(int argc, char *argv[]) {
+	return 0;
 }
